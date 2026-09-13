8db7f9f5d95d09ec59697d7761cadd8b9d6b4ca2
Teja Swaroop <tejaswaroopeddula@gmail.com>
2026-09-13 19:40:57 +0530

    Remove unused S3 application entrypoint

--- backend/app_s3.py
+++
@@ -1,6 +0,0 @@
-"""Production entrypoint that enables the S3 artifact middleware when configured."""
-
-from app import app
-from core.s3_integration import install_storage_integration
-
-install_storage_integration(app)
